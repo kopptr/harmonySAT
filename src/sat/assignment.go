2d21aa85a0f9cb7cbdedb11edd3c3d731240c7bb
Tim Kopp <kopp.tim56@gmail.com>
2012-01-15 21:25:45 -0500

    Finished up assignment

--- src/sat/assignment.go
+++ src/sat/assignment.go
@@ -1,98 +1,108 @@
 package sat
 
 import (
-   "log"
+	"log"
 )
 
+/* A stack of boolean assignments.
+ * Each item on the stack represents a snapshot of the assignment the programmer
+ * may want to return to. One can PushAssign, which makes a snapshot and assigns
+ * the given literal to the next item, or PopAssign and return to the previous
+ * state.
+ */
 type Assignment struct {
-   // top of the stack of assignmentNodes
-   top *assignmentNode
-   // Number of nodes on the stack, not counting the empty assignment.
-   // Also |PushAssign| - |PopAssign|
-   depth uint
+	// top of the stack of assignmentNodes
+	top *assignmentNode
+	// Number of nodes on the stack, not counting the empty assignment.
+	// Also |PushAssign| - |PopAssign|
+	depth uint
 }
 
+// A node in the assignment stack.
 type assignmentNode struct {
-   prev *assignmentNode
-   vars []byte
-   assigned int
+	prev     *assignmentNode
+	vars     []byte
+	assigned int
 }
 
+// Allocates and returns the empty assignment */
 func NewAssignment(nVars int) (a *Assignment) {
-   a = &Assignment{nil,0}
-   a.top = &assignmentNode{nil,nil,0}
-   a.top.vars = make([]byte, nVars)
-   return
+	a = &Assignment{nil, 0}
+	a.top = &assignmentNode{nil, nil, 0}
+	a.top.vars = make([]byte, nVars)
+	return
 }
 
+// Returns the number of items pushed onto the stack
 func (a *Assignment) Depth() uint {
-   return a.depth
+	return a.depth
 }
 
+// Assigns a literal to the top assignment in the stack.
 func (a *Assignment) Assign(l Lit) bool {
-   if a.top == nil {
-      log.Print("Assign() called on uninitialized Assignment")
-      return false
-   }
-   if l.Val < 1 || l.Val > uint(len(a.top.vars)) {
-      log.Printf("Attempted to assign %d (#vars=%d)", l.Val, len(a.top.vars))
-      return false
-   }
-   if a.top.vars[l.Val-1] == Unassigned {
-      a.top.assigned++
-   }
-   a.top.vars[l.Val-1] = l.Pol
-   return true
+	if a.top == nil {
+		log.Print("Assign() called on uninitialized Assignment")
+		return false
+	}
+	if l.Val < 1 || l.Val > uint(len(a.top.vars)) {
+		log.Printf("Attempted to assign %d (#vars=%d)", l.Val, len(a.top.vars))
+		return false
+	}
+	if a.top.vars[l.Val-1] == Unassigned {
+		a.top.assigned++
+	}
+	a.top.vars[l.Val-1] = l.Pol
+	return true
 }
 
 func (a *Assignment) Get(i uint) (Lit, bool) {
-   if a.top == nil {
-      log.Print("Get() called on uninitialized Assignment")
-      return Lit{}, false
-   }
-   if i < 1 || i > uint(len(a.top.vars)) {
-      log.Printf("Attempted to get %d (#vars=%d)", i, len(a.top.vars))
-      return Lit{}, false
-   }
-   return Lit{i, a.top.vars[i-1]}, true
+	if a.top == nil {
+		log.Print("Get() called on uninitialized Assignment")
+		return Lit{}, false
+	}
+	if i < 1 || i > uint(len(a.top.vars)) {
+		log.Printf("Attempted to get %d (#vars=%d)", i, len(a.top.vars))
+		return Lit{}, false
+	}
+	return Lit{i, a.top.vars[i-1]}, true
 }
 
 func (a *Assignment) PushAssign(l Lit) bool {
-   if l.Pol == Unassigned {
-      log.Print("Attempted to PushAssign an unassigned literal\n")
-      return false
-   }
-   if r,e := a.Get(l.Val); r.Pol != Unassigned && e {
-      log.Printf("Attempted to PushAssign a previously assigned %s\n", l)
-      return false
-   }
+	if l.Pol == Unassigned {
+		log.Print("Attempted to PushAssign an unassigned literal\n")
+		return false
+	}
+	if r, e := a.Get(l.Val); r.Pol != Unassigned && e {
+		log.Printf("Attempted to PushAssign a previously assigned %s\n", l)
+		return false
+	}
 
-   newNode := &assignmentNode{nil,nil,0}
-   newNode.prev = a.top
+	newNode := &assignmentNode{nil, nil, 0}
+	newNode.prev = a.top
 
-   newNode.assigned = a.top.assigned
-   newNode.vars = make([]byte, len(a.top.vars))
-   copy(newNode.vars, a.top.vars[:])
+	newNode.assigned = a.top.assigned
+	newNode.vars = make([]byte, len(a.top.vars))
+	copy(newNode.vars, a.top.vars[:])
 
-   a.top = newNode
-   a.depth++
-   return a.Assign(l)
+	a.top = newNode
+	a.depth++
+	return a.Assign(l)
 }
 
 func (a *Assignment) PopAssign() bool {
-   if a.top.prev == nil {
-      log.Printf("Attempted to PopAssign the empty assignment\n")
-      return false
-   }
-   a.top = a.top.prev
-   a.depth--
+	if a.top.prev == nil {
+		log.Printf("Attempted to PopAssign the empty assignment\n")
+		return false
+	}
+	a.top = a.top.prev
+	a.depth--
 
-   return true
+	return true
 }
 
 func (a *Assignment) Initialized() bool {
-   if a.top == nil {
-      return false
-   }
-   return true
+	if a.top == nil {
+		return false
+	}
+	return true
 }
--- src/sat/assignment_test.go
+++ src/sat/assignment_test.go
@@ -1,147 +1,144 @@
 package sat
 
 import (
-   "testing"
+	"testing"
 )
 
 func TestNew(t *testing.T) {
-   var (
-      a Assignment
-      b *Assignment
-   )
-   if a.Initialized() {
-      t.Logf("Assignment reports it is initialized before call to New()\n")
-      t.Fail()
-   }
-   b = NewAssignment(10)
-   if !b.Initialized() {
-      t.Logf("Assignment reports it is uninitialized after call to New()\n")
-      t.Fail()
-   }
-   for i := 0; i < 10; i++ {
-      if b.top.vars[i] != Unassigned {
-         t.Logf("Assignment reports assigned variables after initialization\n")
-         t.Fail()
-      }
-   }
+	var (
+		a Assignment
+		b *Assignment
+	)
+	if a.Initialized() {
+		t.Logf("Assignment reports it is initialized before call to New()\n")
+		t.Fail()
+	}
+	b = NewAssignment(10)
+	if !b.Initialized() {
+		t.Logf("Assignment reports it is uninitialized after call to New()\n")
+		t.Fail()
+	}
+	for i := 0; i < 10; i++ {
+		if b.top.vars[i] != Unassigned {
+			t.Logf("Assignment reports assigned variables after initialization\n")
+			t.Fail()
+		}
+	}
 }
 
 func TestAssign(t *testing.T) {
-   b := NewAssignment(10)
+	b := NewAssignment(10)
 
-   if l,e := b.Get(2); l.Pol != Unassigned || l.Val != 2 || !e {
-      t.Logf("Assign nothing, get 2, it returns %s\n", l)
-      t.Fail()
-   }
+	if l, e := b.Get(2); l.Pol != Unassigned || l.Val != 2 || !e {
+		t.Logf("Assign nothing, get 2, it returns %s\n", l)
+		t.Fail()
+	}
 
-   if e := b.Assign(Lit{2,Pos}); !e {
-      t.Log("Assigning 2 failed\n")
-      t.Fail()
-   }
-   if l,e := b.Get(2); l.Pol != Pos || l.Val != 2 || !e {
-      t.Logf("Assign 2, get, it returns %s\n", l)
-      t.Fail()
-   }
+	if e := b.Assign(Lit{2, Pos}); !e {
+		t.Log("Assigning 2 failed\n")
+		t.Fail()
+	}
+	if l, e := b.Get(2); l.Pol != Pos || l.Val != 2 || !e {
+		t.Logf("Assign 2, get, it returns %s\n", l)
+		t.Fail()
+	}
 
-   if e := b.Assign(Lit{2,Neg}); !e {
-      t.Log("Assigning -2 failed\n")
-      t.Fail()
-   }
-   if l,e := b.Get(2); l.Pol != Neg || l.Val != 2 || !e {
-      t.Logf("Assign -2, get, it returns %s\n", l)
-      t.Fail()
-   }
+	if e := b.Assign(Lit{2, Neg}); !e {
+		t.Log("Assigning -2 failed\n")
+		t.Fail()
+	}
+	if l, e := b.Get(2); l.Pol != Neg || l.Val != 2 || !e {
+		t.Logf("Assign -2, get, it returns %s\n", l)
+		t.Fail()
+	}
 
-   if e := b.Assign(Lit{2,Unassigned}); !e {
-      t.Log("Assigning <2> failed\n")
-      t.Fail()
-   }
-   if l,e := b.Get(2); l.Pol != Unassigned || l.Val != 2 || !e {
-      t.Logf("Assign <2>, get, it returns %s\n", l)
-      t.Fail()
-   }
+	if e := b.Assign(Lit{2, Unassigned}); !e {
+		t.Log("Assigning <2> failed\n")
+		t.Fail()
+	}
+	if l, e := b.Get(2); l.Pol != Unassigned || l.Val != 2 || !e {
+		t.Logf("Assign <2>, get, it returns %s\n", l)
+		t.Fail()
+	}
 
-   if e := b.Assign(Lit{11,Pos}); e {
-      t.Log("Assigning 11 succeeded\n")
-      t.Fail()
-   }
-   if _,e := b.Get(11); e {
-      t.Log("Getting 11 succeeded\n")
-      t.Fail()
-   }
-   if e := b.Assign(Lit{0,Pos}); e {
-      t.Log("Assigning 0 succeeded\n")
-      t.Fail()
-   }
-   if _,e := b.Get(0); e {
-      t.Log("Getting 0 succeeded\n")
-      t.Fail()
-   }
+	if e := b.Assign(Lit{11, Pos}); e {
+		t.Log("Assigning 11 succeeded\n")
+		t.Fail()
+	}
+	if _, e := b.Get(11); e {
+		t.Log("Getting 11 succeeded\n")
+		t.Fail()
+	}
+	if e := b.Assign(Lit{0, Pos}); e {
+		t.Log("Assigning 0 succeeded\n")
+		t.Fail()
+	}
+	if _, e := b.Get(0); e {
+		t.Log("Getting 0 succeeded\n")
+		t.Fail()
+	}
 }
 
 func TestPushPopAssign(t *testing.T) {
-   a := NewAssignment(10)
+	a := NewAssignment(10)
 
-   if a.Depth() != 0 {
-      t.Log("Depth initializes improperly\n")
-      t.Fail()
-   }
-   if l,e := a.Get(2); l.Pol != Unassigned || l.Val != 2 || !e {
-      t.Logf("Initial value is %s\n", l)
-      t.Fail()
-   }
+	if a.Depth() != 0 {
+		t.Log("Depth initializes improperly\n")
+		t.Fail()
+	}
+	if l, e := a.Get(2); l.Pol != Unassigned || l.Val != 2 || !e {
+		t.Logf("Initial value is %s\n", l)
+		t.Fail()
+	}
 
-   a.PushAssign(Lit{2,Pos})
-   if a.Depth() != 1 {
-      t.Log("Depth updates improperly\n")
-      t.Fail()
-   }
-   if l,e := a.Get(2); l.Pol != Pos || l.Val != 2 || !e {
-      t.Logf("PushAssigned 2, value is %s\n", l)
-      t.Logf("val: %d, pol: %d\n", l.Val, l.Pol)
-      t.Fail()
-   }
-   a.PushAssign(Lit{3,Neg})
-   if a.Depth() != 2 {
-      t.Log("Depth updates improperly\n")
-      t.Fail()
-   }
-   if l,e := a.Get(3); l.Pol != Neg || l.Val != 3 || !e {
-      t.Logf("PushAssigned 3, value is %s\n", l)
-      t.Logf("val: %d, pol: %d\n", l.Val, l.Pol)
-      t.Fail()
-   }
-   if l,e := a.Get(2); l.Pol != Pos || l.Val != 2 || !e {
-      t.Logf("PushAssigned 2, value is %s\n", l)
-      t.Logf("val: %d, pol: %d\n", l.Val, l.Pol)
-      t.Fail()
-   }
+	a.PushAssign(Lit{2, Pos})
+	if a.Depth() != 1 {
+		t.Log("Depth updates improperly\n")
+		t.Fail()
+	}
+	if l, e := a.Get(2); l.Pol != Pos || l.Val != 2 || !e {
+		t.Logf("PushAssigned 2, value is %s\n", l)
+		t.Logf("val: %d, pol: %d\n", l.Val, l.Pol)
+		t.Fail()
+	}
+	a.PushAssign(Lit{3, Neg})
+	if a.Depth() != 2 {
+		t.Log("Depth updates improperly\n")
+		t.Fail()
+	}
+	if l, e := a.Get(3); l.Pol != Neg || l.Val != 3 || !e {
+		t.Logf("PushAssigned 3, value is %s\n", l)
+		t.Logf("val: %d, pol: %d\n", l.Val, l.Pol)
+		t.Fail()
+	}
+	if l, e := a.Get(2); l.Pol != Pos || l.Val != 2 || !e {
+		t.Logf("PushAssigned 2, value is %s\n", l)
+		t.Logf("val: %d, pol: %d\n", l.Val, l.Pol)
+		t.Fail()
+	}
 
-   a.PopAssign()
-   if a.Depth() != 1 {
-      t.Log("Depth PopAssigns improperly\n")
-      t.Fail()
-   }
-   if l,e := a.Get(3); l.Pol != Unassigned || l.Val != 3 || !e {
-      t.Logf("PopAssigned value is %s\n", l)
-      t.Fail()
-   }
-   if l,e := a.Get(2); l.Pol != Pos || l.Val != 2 || !e {
-      t.Logf("PushAssigned 2, value is %s\n", l)
-      t.Logf("val: %d, pol: %d\n", l.Val, l.Pol)
-      t.Fail()
-   }
+	a.PopAssign()
+	if a.Depth() != 1 {
+		t.Log("Depth PopAssigns improperly\n")
+		t.Fail()
+	}
+	if l, e := a.Get(3); l.Pol != Unassigned || l.Val != 3 || !e {
+		t.Logf("PopAssigned value is %s\n", l)
+		t.Fail()
+	}
+	if l, e := a.Get(2); l.Pol != Pos || l.Val != 2 || !e {
+		t.Logf("PushAssigned 2, value is %s\n", l)
+		t.Logf("val: %d, pol: %d\n", l.Val, l.Pol)
+		t.Fail()
+	}
 
-   a.PopAssign()
-   if a.Depth() != 0 {
-      t.Log("Depth PopAssigns improperly\n")
-      t.Fail()
-   }
-   if l,e := a.Get(2); l.Pol != Unassigned || l.Val != 2 || !e {
-      t.Logf("PopAssigned value is %s\n", l)
-      t.Fail()
-   }
+	a.PopAssign()
+	if a.Depth() != 0 {
+		t.Log("Depth PopAssigns improperly\n")
+		t.Fail()
+	}
+	if l, e := a.Get(2); l.Pol != Unassigned || l.Val != 2 || !e {
+		t.Logf("PopAssigned value is %s\n", l)
+		t.Fail()
+	}
 }
-
-
-

6b2a2aad8474077c6ecfec4ce6ce23d50ba74aaf
Tim Kopp <kopp.tim56@gmail.com>
2012-01-16 15:48:05 -0500

    Modified lit.go and test
    The cnf module will contain literals that represent true literals. they are
    meant not to be changed after being initialized
    
    The idea of the module as a while is simply to express propositional formulae in
    cnf

--- src/cnf/lit.go
+++ src/cnf/lit.go
@@ -1,46 +1,50 @@
-package sat
+package cnf
 
 import (
-   "strconv"
+	"strconv"
 )
 
-/* Represents a propositional variable, or literal. Val is a unique id for the
- * variable, Pol is it's polarity, Pos(itive), Neg(ative), or Unassigned.
- */
+// Represents a propositional literal. Val is a unique id for the
+// variable, Pol is it's polarity, Pos(itive) or Neg(ative)
 type Lit struct {
-   Val uint
-   Pol byte
+	Val uint
+	Pol byte
 }
 
+// Possible Values for Lit.Pol
 const (
-   Unassigned byte = 0
-   Pos byte = 1
-   Neg byte = 2
+	Pos byte = 1
+	Neg byte = 2
 )
 
+// Returns a Lit given the int representation
+func NewLit(x int) (l Lit) {
+	if x > 0 {
+		l.Val = uint(x)
+		l.Pol = Pos
+	} else if x < 0 {
+		l.Val = uint(x * -1)
+		l.Pol = Neg
+	} // else Lit{0,0} is implicitly set
+	return
+}
+
+// Returns the int representation of a Lit
+func (l *Lit) Int() (x int) {
+	if l.Pol == Pos {
+		x = int(l.Val)
+	} else {
+		x = -1 * int(l.Val)
+	}
+	return
+}
+
+// Returns the string representation of a Lit
 func (l Lit) String() (s string) {
-   if l.Pol == Pos {
-      s = strconv.Uitoa(l.Val)
-   } else if l.Pol == Neg {
-      s = "-" + strconv.Uitoa(l.Val)
-   }
-   return
+	if l.Pol == Pos {
+		s = strconv.Uitoa(l.Val)
+	} else if l.Pol == Neg {
+		s = "-" + strconv.Uitoa(l.Val)
+	}
+	return
 }
-
-/* Returns true if the Lit's polarity is Pos or Neg
- */
-func (l *Lit) IsSet() bool {
-   return l.Pol == Pos || l.Pol == Neg
-}
-
-/* Reverses the polarity of the Lit. If Lit.Pol is unassigned, does nothing.
- */
-func (l *Lit) Flip() {
-   if l.Pol == Pos {
-      l.Pol = Neg
-   } else if l.Pol == Neg {
-      l.Pol = Pos
-   }
-}
-
-

c698b672f38e837b1bc41839365c9df6156df079
Tim Kopp <kopp.tim56@gmail.com>
2012-01-17 00:38:56 -0500

    Cleaned up a bit

--- src/db/cnf/clause.go
+++ src/db/cnf/clause.go
@@ -1,104 +1,103 @@
 package cnf
 
 import (
-   "sort"
-   "bytes"
-   "fmt"
+	"sort"
+	"bytes"
+	"fmt"
 )
 
 // Potential attributes of a clause
 type ClauseStats struct {
-   binary bool // Having exactly two literals
-   ternary bool // Having exactly three literals
-   horn bool // Having <= one positive literal
-   definite bool // Having exactly one positive literal
+	binary   bool // Having exactly two literals
+	ternary  bool // Having exactly three literals
+	horn     bool // Having <= one positive literal
+	definite bool // Having exactly one positive literal
 }
 
 // A clause, or collection of literals. Contains stats about itself that are
 // calculated at creation. Sorted by Lit.Val.
 type Clause struct {
-   ClauseStats
-   Lits []Lit
+	ClauseStats
+	Lits []Lit
 }
 
 // Given a slice of Lits, calculates the stats for them as if they were a
 // clause.
 func (cs *ClauseStats) SetStats(lits []Lit) {
-   cs.binary = false
-   cs.ternary = false
-   cs.horn = false
-   cs.definite = false
-   pos, neg, cnt := 0,0,len(lits)
+	cs.binary = false
+	cs.ternary = false
+	cs.horn = false
+	cs.definite = false
+	pos, neg, cnt := 0, 0, len(lits)
 
-   for _, l := range lits {
-      if l.Pol == Pos {
-         pos++
-      } else {
-         neg++
-      }
-   }
-   if cnt == 2 {
-      cs.binary = true
-   } else if cnt == 3 {
-      cs.ternary = true
-   }
-   if pos <= 1 {
-      cs.horn = true
-      if pos == 1 {
-         cs.definite = true
-      }
-   }
+	for _, l := range lits {
+		if l.Pol == Pos {
+			pos++
+		} else {
+			neg++
+		}
+	}
+	if cnt == 2 {
+		cs.binary = true
+	} else if cnt == 3 {
+		cs.ternary = true
+	}
+	if pos <= 1 {
+		cs.horn = true
+		if pos == 1 {
+			cs.definite = true
+		}
+	}
 }
 
 // Given a collection of integers, converts them into a sorted clause with
 // statistics.
 func NewClause(vars []int) (c *Clause) {
-   c = new(Clause)
-   c.Lits = make([]Lit, len(vars))
-   for i, v := range vars {
-      c.Lits[i] = NewLit(v)
-   }
-   sort.Sort(c)
-   c.SetStats(c.Lits)
-   return
+	c = new(Clause)
+	c.Lits = make([]Lit, len(vars))
+	for i, v := range vars {
+		c.Lits[i] = NewLit(v)
+	}
+	sort.Sort(c)
+	c.SetStats(c.Lits)
+	return
 }
 
 // Returns true if a particular literal is contained in the clause, as well as
 // its polarity.
 func (c *Clause) Contains(val uint) (isContained bool, polarity byte) {
-   in := sort.Search(len(c.Lits), func(i int) bool {
-      return c.Lits[i].Val >= val
-   })
-   if in == len(c.Lits) || c.Lits[in].Val != val {
-      return false, 0
-   }
-   return true, c.Lits[in].Pol
+	// sort.Search returns the lowest index that satisfies the fn
+	in := sort.Search(len(c.Lits), func(i int) bool {
+		return c.Lits[i].Val >= val
+	})
+	if in == len(c.Lits) || c.Lits[in].Val != val {
+		return false, 0
+	}
+	return true, c.Lits[in].Pol
 }
 
 // String representation
 func (c *Clause) String() string {
-   buffer := bytes.NewBufferString("")
-   for _,l := range c.Lits {
-      fmt.Fprintf(buffer, "%s ", l)
-   }
-   fmt.Fprintf(buffer,"\n")
-   return string(buffer.Bytes())
+	buffer := bytes.NewBufferString("")
+	for _, l := range c.Lits {
+		fmt.Fprintf(buffer, "%s ", l)
+	}
+	fmt.Fprintf(buffer, "\n")
+	return string(buffer.Bytes())
 }
 
-/* These functions provide access to the properties of the Clause without
- * granting the ability to change them.
- */
+// These functions provide access to the statistical properties of the Clause
 func (c *Clause) isBinary() bool {
-   return c.binary
+	return c.binary
 }
 func (c *Clause) isTernary() bool {
-   return c.ternary
+	return c.ternary
 }
 func (c *Clause) isHorn() bool {
-   return c.horn
+	return c.horn
 }
 func (c *Clause) isDefinite() bool {
-   return c.definite
+	return c.definite
 }
 
 /* These functions are implemented to implement sort.Interface.
@@ -108,18 +107,17 @@ func (c *Clause) isDefinite() bool {
  */
 // Number of elements in collection
 func (c Clause) Len() int {
-   return len(c.Lits)
+	return len(c.Lits)
 }
 // Compares the literals' values, disregards polarity
 func (c Clause) Less(i, j int) bool {
-   return c.Lits[i].Val < c.Lits[j].Val
+	return c.Lits[i].Val < c.Lits[j].Val
 }
 // Swaps two literals
 func (c Clause) Swap(i, j int) {
-   tmp := Lit{c.Lits[i].Val, c.Lits[i].Pol}
-   c.Lits[i].Val = c.Lits[j].Val
-   c.Lits[i].Pol = c.Lits[j].Pol
-   c.Lits[j].Val = tmp.Val
-   c.Lits[j].Pol = tmp.Pol
+	tmp := Lit{c.Lits[i].Val, c.Lits[i].Pol}
+	c.Lits[i].Val = c.Lits[j].Val
+	c.Lits[i].Pol = c.Lits[j].Pol
+	c.Lits[j].Val = tmp.Val
+	c.Lits[j].Pol = tmp.Pol
 }
-
--- src/db/cnf/clause_test.go
+++ src/db/cnf/clause_test.go
@@ -1,80 +1,80 @@
 package cnf
 
 import (
-   "testing"
+	"testing"
 )
 
 func TestNewClause(t *testing.T) {
-   is1 := []int{-3,2, -5, 1}
-   c1 := NewClause(is1)
-   if !sameStats(c1.ClauseStats, ClauseStats{false,false,false,false}) {
-      t.Log("c1's stats are incorrect\n")
-      t.Fail()
-   }
+	is1 := []int{-3, 2, -5, 1}
+	c1 := NewClause(is1)
+	if !sameStats(c1.ClauseStats, ClauseStats{false, false, false, false}) {
+		t.Log("c1's stats are incorrect\n")
+		t.Fail()
+	}
 
-   is2 := []int{-3,-5, 1}
-   c2 := NewClause(is2)
-   if !sameStats(c2.ClauseStats, ClauseStats{false,true,true,true}) {
-      t.Log("c2's stats are incorrect\n")
-      t.Fail()
-   }
+	is2 := []int{-3, -5, 1}
+	c2 := NewClause(is2)
+	if !sameStats(c2.ClauseStats, ClauseStats{false, true, true, true}) {
+		t.Log("c2's stats are incorrect\n")
+		t.Fail()
+	}
 
-   is3 := []int{-3, -86,-5, 1}
-   c3 := NewClause(is3)
-   if !sameStats(c3.ClauseStats, ClauseStats{false,false,true,true}) {
-      t.Log("c3's stats are incorrect\n")
-      t.Fail()
-   }
+	is3 := []int{-3, -86, -5, 1}
+	c3 := NewClause(is3)
+	if !sameStats(c3.ClauseStats, ClauseStats{false, false, true, true}) {
+		t.Log("c3's stats are incorrect\n")
+		t.Fail()
+	}
 
-   is4 := []int{-3,-5}
-   c4 := NewClause(is4)
-   if !sameStats(c4.ClauseStats, ClauseStats{true,false,true,false}) {
-      t.Log("c4's stats are incorrect\n")
-      t.Fail()
-   }
+	is4 := []int{-3, -5}
+	c4 := NewClause(is4)
+	if !sameStats(c4.ClauseStats, ClauseStats{true, false, true, false}) {
+		t.Log("c4's stats are incorrect\n")
+		t.Fail()
+	}
 }
 
 func TestContains(t *testing.T) {
-   c1 := NewClause([]int{-3,2, -5, 1, 10})
+	c1 := NewClause([]int{-3, 2, -5, 1, 10})
 
-   in1, pol1 := c1.Contains(2)
-   if !in1 {
-      t.Log("c1 reports that it does not contain 2\n")
-      t.Fail()
-   }
-   if pol1 != Pos {
-      t.Log("c1 reports having 2 in the wrong polarity\n")
-   }
-   in2, pol2 := c1.Contains(3)
-   if !in2 {
-      t.Log("c1 reports that it does not contain 3\n")
-      t.Fail()
-   }
-   if pol2 != Neg {
-      t.Log("c1 reports having 3 in the wrong polarity\n")
-   }
-   in3, pol3 := c1.Contains(4)
-   if in3 {
-      t.Log("c1 reports that it contains 4\n")
-      t.Fail()
-   }
-   if pol3 != 0 {
-      t.Log("c1 reports a polarity for 4\n")
-   }
+	in1, pol1 := c1.Contains(2)
+	if !in1 {
+		t.Log("c1 reports that it does not contain 2\n")
+		t.Fail()
+	}
+	if pol1 != Pos {
+		t.Log("c1 reports having 2 in the wrong polarity\n")
+	}
+	in2, pol2 := c1.Contains(3)
+	if !in2 {
+		t.Log("c1 reports that it does not contain 3\n")
+		t.Fail()
+	}
+	if pol2 != Neg {
+		t.Log("c1 reports having 3 in the wrong polarity\n")
+	}
+	in3, pol3 := c1.Contains(4)
+	if in3 {
+		t.Log("c1 reports that it contains 4\n")
+		t.Fail()
+	}
+	if pol3 != 0 {
+		t.Log("c1 reports a polarity for 4\n")
+	}
 }
 
 func sameStats(a, b ClauseStats) bool {
-   if a.binary != b.binary {
-      return false
-   }
-   if a.ternary != b.ternary {
-      return false
-   }
-   if a.horn != b.horn {
-      return false
-   }
-   if a.definite != b.definite {
-      return false
-   }
-   return true
+	if a.binary != b.binary {
+		return false
+	}
+	if a.ternary != b.ternary {
+		return false
+	}
+	if a.horn != b.horn {
+		return false
+	}
+	if a.definite != b.definite {
+		return false
+	}
+	return true
 }

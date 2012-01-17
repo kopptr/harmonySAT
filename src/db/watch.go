ed616c4ba9f6456665048b4442a106372d49a210
Tim Kopp <kopp.tim56@gmail.com>
2012-01-17 13:56:29 -0500

    Watch is publicly exported.
    Bcp/Dpll will need it

--- src/db/watch.go
+++ src/db/watch.go
@@ -1,18 +1,22 @@
 package db
 
-type watch struct {
-   next *watch
-   prev *watch
-   watching Lit
+import (
+   "cnf"
+)
+
+type Watch struct {
+   next *Watch
+   prev *Watch
+   watching cnf.Lit
 }
 
-func (w *watch) New() {
+func (w *Watch) New() {
    w.next = w
    w.prev = w
    w.watching.Val = 0
-   w.watching.Pol = Unassigned
+   w.watching.Pol = 0
 }
 
-func (w *watch) isDummy() bool {
+func (w *Watch) isDummy() bool {
    return w.watching.Val == 0
 }

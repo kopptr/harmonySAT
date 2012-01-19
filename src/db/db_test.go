3ba475df39b0ed8b8ebcfd598f06f681dfb21052
Tim Kopp <kopp.tim56@gmail.com>
2012-01-18 20:14:31 -0500

    More testing

--- src/db/db_test.go
+++ src/db/db_test.go
@@ -42,21 +42,71 @@ func TestNewDB(t *testing.T) {
 func TestAddEntry(t *testing.T) {
    db := NewDB(10)
    db.AddEntry([]int{-1,3,-5})
-   if db.NLearned() != 0 || db.NGiven() != 1 {
+   if db.NGiven() != 1 || db.NLearned() != 0 {
       t.Logf("entry totals update incorrectly\n")
+      t.Logf("NGiven(1) = %d, NLearned(0) = %d\n", db.NGiven(), db.NLearned())
       t.Fail()
    }
-   fmt.Printf("%s\n", db.String() )
+   //fmt.Printf("%s\n", db.String() )
    db.AddEntry([]int{-10,4,-5,6,1})
-   fmt.Printf("%s\n", db.String() )
+   if db.NGiven() != 2 || db.NLearned() != 0 {
+      t.Logf("entry totals update incorrectly after entry add 1\n")
+      t.Logf("NGiven(2) = %d, NLearned(0) = %d\n", db.NGiven(), db.NLearned())
+      t.Fail()
+   }
+   //fmt.Printf("%s\n", db.String() )
+
    db.AddEntry([]int{-9,4,-5,6,1})
-   fmt.Printf("%s\n", db.String() )
+   if db.NGiven() != 3 || db.NLearned() != 0 {
+      t.Logf("entry totals update incorrectly after entry add 2\n")
+      t.Logf("NGiven(3) = %d, NLearned(0) = %d\n", db.NGiven(), db.NLearned())
+      t.Fail()
+   }
+   //fmt.Printf("%s\n", db.String() )
 
    db.StartLearning()
    db.AddEntry([]int{-9,-4,-5,6,1})
-   fmt.Printf("%s\n", db.String() )
+   if db.NGiven() != 3 || db.NLearned() != 1 {
+      t.Logf("entry totals update incorrectly after entry add 3\n")
+      t.Logf("NGiven(3) = %d, NLearned(1) = %d\n", db.NGiven(), db.NLearned())
+      t.Fail()
+   }
+   //fmt.Printf("%s\n", db.String() )
+
    db.AddEntry([]int{-9,4,-3,6,1})
-   fmt.Printf("%s\n", db.String() )
+   if db.NGiven() != 3 || db.NLearned() != 2 {
+      t.Logf("entry totals update incorrectly after entry add 4\n")
+      t.Logf("NGiven(3) = %d, NLearned(2) = %d\n", db.NGiven(), db.NLearned())
+      t.Fail()
+   }
+   //fmt.Printf("%s\n", db.String() )
    db.AddEntry([]int{-9,4,-5,2,1})
+   if db.NGiven() != 3 || db.NLearned() != 3 {
+      t.Logf("entry totals update incorrectly after entry add 5\n")
+      t.Logf("NGiven(3) = %d, NLearned(3) = %d\n", db.NGiven(), db.NLearned())
+      t.Fail()
+   }
+   //fmt.Printf("%s\n", db.String() )
+
+   // TODO check the stats
+}
+
+func TestDelEntry(t *testing.T) {
+   db := NewDB(10)
+   db.AddEntry([]int{-1,3,-5})
+   db.AddEntry([]int{-10,4,-5,6,1})
+   db.AddEntry([]int{-9,4,-5,6,1})
+   db.StartLearning()
+   db.AddEntry([]int{-9,-4,-5,6,1})
+   db.AddEntry([]int{-9,4,-3,6,1})
+   db.AddEntry([]int{-9,4,-5,2,1})
+
    fmt.Printf("%s\n", db.String() )
+
+
+
+
+
+
+
 }

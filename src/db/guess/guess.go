24b832fd2050cfeb7eae81921a1d6cceef0e02fc
Tim Kopp <kopp.tim56@gmail.com>
2012-01-21 13:23:42 -0500

    Decided to split the assignment into two
    Now, we can implement BCP at the clause-db level, which is nice.

---
+++ src/db/guess/guess.go
@@ -0,0 +1,31 @@
+package guess
+
+const (
+   Unassigned byte = 0
+   Pos byte = 1
+   Neg byte = 2
+)
+
+// Guess is a struct because we want to create a thin abstraction over the fact
+// that it's just a simple array. This way, the user doesn't need to worry about
+// subtracting 1. Silly, but I'm okay with it.
+type Guess struct {
+   vars []byte
+}
+
+func NewGuess(nVars int) (g *Guess) {
+   g.vars = make([]byte, nVars)
+   return
+}
+
+// Sets the variable n. v must be \in {Unassigned,Pos,Neg}
+func (g *Guess) Set(n int, v byte) {
+   g.vars[n-1] = v
+}
+
+// Returns what is assigned to the nth variable.
+func (g *Guess) Get(n int) {
+   return g.vars[n-1]
+}
+
+

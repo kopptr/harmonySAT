4881c2e09cedf77a66d2606df6015e18be9a1557
Tim Kopp <kopp.tim56@gmail.com>
2012-01-30 18:38:41 -0500

    Added dbms's to the test script

--- test/test.sh
+++ test/test.sh
@@ -4,17 +4,20 @@
 
 LIST=`ls test/*.cnf`
 BRANCH_RULES="ordered random vsids"
+CDBMS="none queue"
 PASS=true
 
 for file in ${LIST}; do
-        for rule in ${BRANCH_RULES} ; do
-           echo -n "${file}, ${rule}: "
-           RES=`./bin/hsat -q -file=./${file} -branch=${rule}`
-           if [ "${RES}" != "SAT" ]; then
-              PASS=false
-           fi
-           echo $RES
-        done
+  for rule in ${BRANCH_RULES} ; do
+     for cdbms in ${CDBMS}; do
+        echo -n "${file}, ${rule}, ${cdbms}: "
+        RES=`./bin/hsat -q -file=./${file} -branch=${rule} -dbms=${cdbms}`
+        if [ "${RES}" != "SAT" ]; then
+           PASS=false
+        fi
+        echo $RES
+     done
+  done
 done
 
 if $PASS; then

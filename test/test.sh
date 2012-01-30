9361c388a766bbedc1bf9a9dbf47f922dfdfb16d
kopptr <kopp.tim56@gmail.gom>
2012-01-30 14:32:07 -0500

    Added other branching rules to test script

--- test/test.sh
+++ test/test.sh
@@ -3,15 +3,18 @@
 `go install hsat`
 
 LIST=`ls test/*.cnf`
+BRANCH_RULES="ordered random vsids"
 PASS=true
 
 for file in ${LIST}; do
-   echo -n "${file}: "
-   RES=`./bin/hsat -q -file=./${file}`
-   if [ "${RES}" != "SAT" ]; then
-      PASS=false
-   fi
-   echo $RES
+        for rule in ${BRANCH_RULES} ; do
+           echo -n "${file}, ${rule}: "
+           RES=`./bin/hsat -q -file=./${file} -branch=${rule}`
+           if [ "${RES}" != "SAT" ]; then
+              PASS=false
+           fi
+           echo $RES
+        done
 done
 
 if $PASS; then

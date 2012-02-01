95c1454349f1968026cc1ea97f4d06c72f42c30f
Tim Kopp <kopp.tim56@gmail.com>
2012-01-31 21:45:01 -0500

    Implemented bench.sh
    Runs the benchmarks and makes them into pretty tables.

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
 go install hsat
-FILE=$1
 
-./bin/hsat -cpuprofile=cpu.prof -branch=vsids -dbms=queue -file=${FILE}
+#BENCH=`ls benchmark/*.cnf`
+BENCH=`ls test/*.cnf`
+BRANCH="ordered random vsids moms"
+DBMS="none queue berkmin"
+
+echo "\\documentclass{article}"
+echo "\\usepackage{geometry}"
+echo "\\usepackage{morefloats}"
+echo ""
+echo "\\title{Base Solver Measurements}"
+echo "\\author{Tim Kopp}"
+echo "\\date{`date +%D`}"
+echo ""
+echo "\\begin{document}"
+echo ""
+
+for bench in ${BENCH}; do
+   echo "\\begin{table}[ht!]"
+   echo "\\centering"
+   echo "\\begin{tabular}{|c|c||c|}\\hline"
+   echo "Branch & DBMS & Time\\\\\\hline\\hline"
+   for rule in ${BRANCH}; do
+      for dbms in ${DBMS}; do
+         echo -n "${rule} & ${dbms} & "
+         RES=`ulimit -t 1200; /usr/bin/time -f %E ./bin/hsat -q -file=${bench} -branch=${rule} -dbms=${dbms} 2>&1 >/dev/null`
+         echo -n ${RES}
+         echo "\\\\\\hline"
+      done
+   done
+   echo "\\end{tabular}"
+   echo "\\caption{${bench}}"
+   echo "\\label{tab:${bench}}"
+   echo "\\end{table}"
+   echo ""
+done
+
+echo "\\end{document}"

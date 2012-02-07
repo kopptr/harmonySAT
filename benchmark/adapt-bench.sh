ce44e52620ace5eb8a1920f8af1ea511a5bd82a7
Tim Kopp <kopp.tim56@gmail.com>
2012-02-07 14:23:44 -0500

    Script for running adaptive solver.

---
+++ benchmark/adapt-bench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+go install hsat
+
+BENCH=`ls benchmark/in-use/*.cnf`
+JSON=$1
+rm -f output.txt
+
+for bench in ${BENCH}; do
+   echo "./bin/hsat -file=${bench} -a=${JSON}" >> output.txt
+   ./bin/hsat -file=${BENCH} -a=${JSON} >> output.txt
+   echo "" >> output.txt
+done
